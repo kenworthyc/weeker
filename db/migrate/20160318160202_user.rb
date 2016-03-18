23577be27110f6ef14bf3d86a14cec689735051e
Caroline <kenworthyc@gmail.com>
2016-03-18 16:19:21 -0500

    Add migration and model with BCrypt for user

---
+++ db/migrate/20160318160202_user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Migration
+  def change
+      def change
+        create_table :users do |t|
+          t.string :first_name
+          t.string :last_name
+          t.string :email null:false
+          t.string :password_digest null:false
+          t.timestamps null: false
+        end
+      end
+  end
+end
