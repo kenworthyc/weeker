41e01af5ec436c707f38240bb3f3a045a011b451
Caroline <kenworthyc@gmail.com>
2016-03-18 16:19:21 -0500

    Add migration and model with BCrypt for user

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Remember to create a migration!
+
+  include BCrypt
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :password, presence: true
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user
+      return @user if @user.password == password
+    end
+    nil
+  end
+
+end
