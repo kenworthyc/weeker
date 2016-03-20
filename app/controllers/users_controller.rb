8ba42754909d7e045c86444dbbbc60e9e8aa9da8
Caroline <kenworthyc@gmail.com>
2016-03-20 00:12:21 -0500

    Add controller with current user checks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,28 @@ end
 
 get '/users/new/?' do
   @user = User.new
-  erb :'users/new'
+
+  errors = @user.errors.full_messages
+  erb :'users/new', locals: { errors: errors }
 end
 
 post '/users/?' do
   @user = User.new(params[:user])
 
   if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
   else
     @user
     errors = @user.errors.full_messages
 
     erb :'users/new', locals: { errors: errors }
   end
+end
+
+get '/users/:id/?' do
+    @user = User.find(params[:id])
+    redirect "/" unless current_user.id == @user.id
+
+    erb :'users/show'
 end
