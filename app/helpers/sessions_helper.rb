79f4bc473291483289edb5bbad06618a87b80a6c
pdynowski <paul.dynowski@gmail.com>
2016-03-31 11:55:46 -0500

    Store twitter access token info to db

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,16 +13,30 @@ helpers do
 															 { site: base } )
 
 		request_token = consumer.get_request_token(oauth_callback: callback)
-		puts request_token.inspect
 		session[:request_token] = request_token
 		redirect request_token.authorize_url
   end
 
   def get_twitter_access_token
-  	puts params
-  	puts session[:request_token]
-  	access_token = session[:request_token].get_access_token(oauth_verifier: params[:oauth_verifier])
-  	puts access_token.inspect
+  	session[:access_token] = access_token = session[:request_token].get_access_token(oauth_verifier: params[:oauth_verifier])
+  end
+
+  def store_twitter_access_token
+  	id = session[:user_id]
+  	token = session[:access_token].token
+  	secret = session[:access_token].secret
+  	destination = Destination.find_by(user_id: id)
+  	if destination
+  		destination.twitter_secret = secret
+  		destination.twitter_token = token
+  		destination.save
+  	else
+  		Destination.create(user_id: id, twitter_secret: secret, twitter_token: token)
+  	end
+  end
+
+  def send_tweet
+  	
   end
 
 end
