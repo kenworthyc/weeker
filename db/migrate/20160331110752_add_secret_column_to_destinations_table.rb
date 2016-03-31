91aeed6b5e82b547c492d4eb452057b753d647ed
pdynowski <paul.dynowski@gmail.com>
2016-03-31 11:32:41 -0500

    Work on DB migrations

--- db/migrate/20160331110752_add_secret_column_to_destinations_table.rb
+++ db/migrate/20160331110752_add_secret_column_to_destinations_table.rb
@@ -1,5 +1,5 @@
 class AddSecretColumnToDestinationsTable < ActiveRecord::Migration
   def change
-  	add_column :destinations, :secret, :string
+  	add_column :destinations, :twitter_secret, :string
   end
 end
--- db/migrate/20160331112031_rename_twitter_token_to_token_on_destinations.rb
+++
@@ -1,5 +0,0 @@
-class RenameTwitterTokenToTokenOnDestinations < ActiveRecord::Migration
-  def change
-  	rename_column :destinations, :twitter_token, :token
-  end
-end
