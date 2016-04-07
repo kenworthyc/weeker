b06eeac469754fb9e36663ce46f2779d19622e62
bobdorff <robertdorff@gmail.com>
2016-04-07 14:56:03 -0500

    move services controller into separate sources and dests

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,5 +1,4 @@
 require 'dropbox_sdk'
-require 'twitter'
 
 get '/sources/new' do 
   @user = current_user 
@@ -37,19 +36,3 @@ get '/sources/dropbox-complete' do
   erb :'sources/new'
 end
 
-#Twitter Services
-
-get '/twitter-authenticate' do
-	get_twitter_info
-end
-
-get '/twitter-authentication-return' do
-	get_twitter_access_token
-	store_twitter_access_token
-	redirect "/users/#{session[:user_id]}"
-end
-
-get '/test-tweet' do
-	send_tweet(User.find(session[:user_id]), twitter_media_upload )
-	redirect "/users/#{session[:user_id]}"
-end
