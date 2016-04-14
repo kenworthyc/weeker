6dd988951986d5dc1cfbe375208860bc88b90731
tommchenry <tommchenry@gmail.com>
2016-04-14 17:48:20 -0500

    Create weekly folder in archive.

--- app/helpers/dropbox_helper.rb
+++ app/helpers/dropbox_helper.rb
@@ -12,13 +12,32 @@ helpers do
 		 DropboxOAuth2Flow.new( ENV['DROPBOX_KEY'],	ENV['DROPBOX_SECRET'], ENV['DROPBOX_CALLBACK'], session, :dropbox_token)
 	end
 
+  def create_weekly_folder(client)
+
+  end
+
   def move_dropbox_file(client, file_url, destination_url)
-    puts file_url
-    puts destination_url
+    #puts file_url
+    #puts destination_url
     client.file_move(file_url, destination_url)
   end
 
+  def name_archive_folder
+    require 'date'
+    now = Date.today
+    week_ago = now - 7
+    today = Time.now.strftime("%Y-%m-%d")
+    "#{week_ago}_to_#{today}"
+  end
+
+  def create_archive_folder(client)
+    path = "/archive/" + name_archive_folder 
+    #add test for existence
+    client.file_create_folder(path)
+  end
+
   def tweet_all_images_in_folder(client)
+    create_archive_folder(client)
     client.metadata('/this-week')["contents"].each do |image|
       image_path = image["path"]
       puts "image_path #{image_path}"
@@ -28,7 +47,7 @@ helpers do
       #twitter_media_upload("This is something:", dropbox_url)
       puts "Posted #{dropbox_url}"
       destination_url = image_path.gsub(/\/this-week/,'/archive')
-      move_dropbox_file(client, image_path, destination_url)
+      #move_dropbox_file(client, image_path, destination_url)
       sleep 5
     end
   end
